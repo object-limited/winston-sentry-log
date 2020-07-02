a3acfd74c3ad384071a4bf2b898ae077166f59fc
Maxwell Ainatchi <maxwell.ainatchi@gmail.com>
2020-07-02 17:01:53 -0400

    ditto

--- dist/NestedError.js
+++ dist/NestedError.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class NestedError extends Error {
     constructor(error, message) {
-        super(message ?? error.message);
+        super(message || error.message);
         this.nestedError = error;
     }
 }
--- src/NestedError.ts
+++ src/NestedError.ts
@@ -2,7 +2,7 @@ export class NestedError<T extends Error> extends Error {
   public nestedError: T;
 
   constructor(error: T, message?: string) {
-    super(message ?? error.message);
+    super(message || error.message);
     this.nestedError = error;
   }
 }
